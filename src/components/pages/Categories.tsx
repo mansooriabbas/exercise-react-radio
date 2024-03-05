897df9c319f3748876646674773f857587cf22b5
mansooriabbas <mansoori.abbas@outlook.com>
2024-03-06 00:59:47 +0100

    styles the component

--- src/components/pages/Categories.tsx
+++ src/components/pages/Categories.tsx
@@ -1,8 +1,10 @@
+import "./Categories.css";
 import { Navbar } from "../Navbar/Navbar";
 import { useState } from "react";
 export const Categories = (props) => {
   const { programcategories } = props.categorieData;
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+  const [additionalData, setAdditionalData] = useState(null);
 
   const handleCategoryClick = async (categoryId) => {
     try {
@@ -10,6 +12,7 @@ export const Categories = (props) => {
         `https://api.sr.se/api/v2/programs/index?programcategoryid=${categoryId}&format=json`
       );
       const additionalData = await response.json();
+      setAdditionalData(additionalData);
 
       console.log(
         "Additional data for category ID",
@@ -25,15 +28,50 @@ export const Categories = (props) => {
   return (
     <>
       <Navbar />
-      <ul>
-        {programcategories &&
-          programcategories.map((cat, index) => (
-            <li key={index} onClick={() => handleCategoryClick(cat.id)}>
-              {cat.name}
-            </li>
-          ))}
-      </ul>
-      <ul></ul>
+      <div className="cat-container">
+        <ul>
+          {programcategories &&
+            programcategories.map((cat, index) => (
+              <li key={index} onClick={() => handleCategoryClick(cat.id)}>
+                {cat.name}
+              </li>
+            ))}
+        </ul>
+      </div>
+      {/* {console.log(additionalData.programs, "HEYYYYY")} */}
+
+      {additionalData && (
+        <div className="additional-data-container">
+          <h2> {additionalData.programs[0].programcategory.name}</h2>
+
+          <ul>
+            {additionalData.programs.map((dataItem, index) => (
+              <li key={index}>
+                <img src={dataItem.programimage} alt="" />
+                <div className="text-container">
+                  <p>
+                    <span>Channel</span>:{" "}
+                    {dataItem.channel.name.includes("No channel")
+                      ? "Channel info not available"
+                      : dataItem.channel.name}
+                  </p>
+                  <p>
+                    <span>Description</span>: {dataItem.description}
+                  </p>
+
+                  <p>
+                    <span>Broadcast Information</span>: {dataItem.broadcastinfo}
+                  </p>
+
+                  <p>
+                    <span>Category</span>: {dataItem.programcategory.name}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 };
